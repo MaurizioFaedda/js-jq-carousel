e9b701d16d5143c079488b2b7377b34559b90823
Maurizio <maurizio.faedda1@gmail.com>
2020-10-27 16:47:08 +0100

    Creata condizione per scorrere le img di continuo avanti e indietro

--- main.js
+++ main.js
@@ -7,17 +7,27 @@ $(document).ready(function() {
 
         imgCorrente.removeClass('active');
 
-        imgCorrente.next('img').addClass('active');
+        // aggiungo la classe active solo dopo aver verificato che ci sia un'img (quindi il valore length dev'essere almeno 1). Quando questa condizione non si verifica (-nessun elemento- quindi length pari a 0), aggiungo la classe active alla mia prima img(figlio).
+
+        if(imgCorrente.next('img').length) {
+            imgCorrente.next('img').addClass('active');
+        } else {
+            $('.content-box :first-child').addClass('active')
+        }
     });
 
-    // quando clicco nella freccia dx voglio rimuovere la classe active all'elemento corrente e aggiungere la classe active all'elemento successivo
+    // quando clicco nella freccia sx voglio rimuovere la classe active all'elemento corrente e aggiungere la classe active all'elemento successivo
     $('.arrow-left > i').click(function() {
 
         var imgCorrente = $('img.active');
 
         imgCorrente.removeClass('active');
 
-        imgCorrente.prev('img').addClass('active');
+        if(imgCorrente.prev('img').length) {
+            imgCorrente.prev('img').addClass('active');
+        } else {
+            $('.content-box img:last-child').addClass('active')
+        }
     });
 
 })
