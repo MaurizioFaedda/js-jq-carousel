04cd918720a2422b49ee760d5c0eb1b8cc5c21ff
Maurizio <maurizio.faedda1@gmail.com>
2020-10-27 17:15:12 +0100

    Creati indicatori circle che cambiano con lo scorrere delle img

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ $(document).ready(function() {
     $('.arrow-right > i').click(function() {
 
         var imgCorrente = $('img.active');
+        var circleCorrente = $('.circle > .fas')
 
         imgCorrente.removeClass('active');
+        circleCorrente.removeClass('fas')
 
         // aggiungo la classe active solo dopo aver verificato che ci sia un'img (quindi il valore length dev'essere almeno 1). Quando questa condizione non si verifica (-nessun elemento- quindi length pari a 0), aggiungo la classe active alla mia prima img(figlio).
 
@@ -14,19 +16,33 @@ $(document).ready(function() {
         } else {
             $('.content-box :first-child').addClass('active')
         }
+        if(circleCorrente.next('i').length) {
+            circleCorrente.next('i').addClass('fas');
+        } else {
+            $('.circle :first-child').addClass('fas')
+        }
     });
 
     // quando clicco nella freccia sx voglio rimuovere la classe active all'elemento corrente e aggiungere la classe active all'elemento successivo
     $('.arrow-left > i').click(function() {
 
         var imgCorrente = $('img.active');
+        var circleCorrente = $('.circle > .fas')
+
 
         imgCorrente.removeClass('active');
+        circleCorrente.removeClass('fas')
+
 
         if(imgCorrente.prev('img').length) {
             imgCorrente.prev('img').addClass('active');
         } else {
             $('.content-box img:last-child').addClass('active')
+        }
+        if(circleCorrente.prev('i').length) {
+            circleCorrente.prev('i').addClass('fas');
+        } else {
+            $('.circle i:last-child').addClass('fas')
         }
     });
 
